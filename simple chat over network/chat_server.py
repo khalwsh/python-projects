c0c3775e7edad8670f48b6c5eeefea2ff282b6f3
khaled <136606858+khalwsh@users.noreply.github.com>
2024-05-19 03:40:18 +0300

    Update chat_server.py

--- simple chat over network/chat_server.py
+++ simple chat over network/chat_server.py
@@ -1,12 +1,12 @@
 import socket
 import threading
 
-server = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = '127.0.0.1'
 port = 4010
 
-server.bind((host , port))
+server.bind((host, port))
 
 server.listen(5)
 
@@ -14,11 +14,23 @@ clients = []
 
 nicknames = []
 
-def broadcast(message):    
+def broadcast(message):
+    """
+    Broadcasts a message to all connected clients.
+
+    Parameters:
+    message (bytes): The message to be broadcasted.
+    """
     for client in clients:
         client.send(message)
-        
+
 def handle(client):
+    """
+    Handles messages received from a client.
+
+    Parameters:
+    client (socket.socket): The client socket object.
+    """
     while True:
         try:
             message = client.recv(2048)
@@ -31,15 +43,18 @@ def handle(client):
             nicknames.remove(nickname)
             break
 
-
-def recieve():
+def receive():
+    """
+    Listens for incoming connections and handles them.
+    """
     while True:
-        client , address = server.accept()
+        client, address = server.accept()
         client.send("NickName".encode("utf-8"))
         nickname = client.recv(2048).decode("utf-8")
         nicknames.append(nickname)
         clients.append(client)
         broadcast(f"{nickname} connected to server\n".encode("utf-8"))
-        thread = threading.Thread(target = handle , args = (client,))
+        thread = threading.Thread(target=handle, args=(client,))
         thread.start()
-recieve()
+
+receive()
